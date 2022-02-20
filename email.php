e3980653e58ecc1ebfbc882eeb0787f6f63564a5
ai22ak47 <72119089+Sabbir298@users.noreply.github.com>
2022-02-21 00:51:30 +0600

    Add files via upload

---
+++ email.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>EMAIL SPOOFING | TOXIC-VIRUS</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel = "icon" href = "/logo.png" type = "image/x-icon">
+</head>
+<style>
+img[src*="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"] {  display:none;}
+body{
+  background-color: black;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+slog{
+  color: yellowgreen;
+  font-size: 20px;
+  font-family: 'Syne Tactile', cursive;
+}
+h1 {
+  font-family: 'Ubuntu', sans-serif;
+  color: greenyellow;
+  text-shadow: red 2px;
+  font-size: 40px;
+}
+h2{
+  color: lawngreen;
+}
+h3{
+  color: greenyellow;
+  font-size: 40px;
+}
+hr.x {
+  border-top: 2px dashed greenyellow;
+}
+label{
+  color: greenyellow;
+}
+
+input[type=text] {
+  width: 100%;
+  padding: 10px 20px;
+  margin: 20px 0;
+  font-size: 17px;
+  box-sizing: border-box;
+  border-color: red;
+  border-radius: 20px;
+  background-color: #d9d9d9;
+  color: black;
+}
+::placeholder {
+  color: black;
+  opacity: 60%; 
+  font-size: 17px;
+}
+.button{
+  background-color: greenyellow;
+  color: black;
+  font-size: 20px;
+  padding: 10px 50px;
+  border: red;
+  cursor: pointer;
+  border-radius: 50px;
+  text-align: center;
+}
+.button:hover{
+  background-color: black;
+  color: greenyellow;
+  cursor: pointer;
+}
+</style>
+</head>
+<body>
+  <div align="center">
+  <a href="index.html"><h1><b>☣TOXIC-VIRUS☣</h1></a>
+  <slog>Make Google Your Best Friend .....!!</slog>
+  <br><br>
+  </div>
+  <h2>~/ Hello World !!</h2>
+  <hr class="x">
+  <div>
+<h3 style="margin-right: 90%"><\<h3>
+  </div>
+<form action="" method="post">
+  <label for="fname">~/ VICTIM EMAIL</label>
+  <input type="text" name="email" placeholder="victim@txvirus.com">
+  
+  <label for="text">~/ ANONYMOUS NAME</label>
+  <input type="text" name="name" placeholder="TOXIC-VIRUS">
+  
+  <label for="text">~/ SENDER EMAIL</label>
+  <input type="text" name="sender" placeholder="fakemail@txvirus.com">
+  
+  <label for="text">~/ SUBJECT</label>
+  <input type="text" name="subject" placeholder="EMAIL SUBJECT">
+  
+  <label for="text">~/ MESSAGE</label><br>
+  <input type="text" name="content" placeholder="YOUR MESSAGE............."><br><br>
+  
+   <input class="button" type="submit" value="Submit">
+</form>
+<h3 style="margin-left: 90%">/><h3>
+    <hr class="x">
+  
+  <div align="center">
+   
+    <a href="https://facebook.com/t0xicvirus"><i class="fa fa-facebook" style="font-size:30px;color: greenyellow"></i></a>
+    
+    <span> | </span>
+    
+    <a href="https://t.me/txvirus"><i class="fa fa-telegram" style="font-size:30px;color: greenyellow"></i></a>
+    
+    <span> | </span>
+    
+    <a href="https://github.com/AKXVAU"><i class="fa fa-github" style="font-size:30px;color: greenyellow"></i></a>
+    
+    <span> | </span>
+    <a href="contact.php"><i class="fa fa-envelope" style="font-size:30px;color: greenyellow"></i></a>
+    
+    </div>
+
+    <div align="right">
+    <a href="https://t.me/txvirus" title="website hit counter"><img src="https://counter2.stat.ovh/private/freecounterstat.php?c=bjx8k5srj89r6eck5xf4sypult4bc56a" border="0" title="website hit counter" alt="SITE VISITORS"></a>
+    </div>
+ 
+<div style="text-align: right;position: fixed;z-index:9999999;bottom: 0;width: auto;right: 1%;cursor: pointer;line-height: 0;display:block !important;"><a title="Hosted on free web hosting 000webhost.com. Host your own website for FREE." target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&utm_campaign=000_logo&utm_medium=website&utm_content=footer_img"><img src="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png" alt="www.000webhost.com"></a></div><script>function getCookie(t){for(var e=t+"=",n=decodeURIComponent(document.cookie).split(";"),o=0;o<n.length;o++){for(var i=n[o];" "==i.charAt(0);)i=i.substring(1);if(0==i.indexOf(e))return i.substring(e.length,i.length)}return""}getCookie("hostinger")&&(document.cookie="hostinger=;expires=Thu, 01 Jan 1970 00:00:01 GMT;",location.reload());var wordpressAdminBody=document.getElementsByClassName("wp-admin")[0],notification=document.getElementsByClassName("notice notice-success is-dismissible"),hostingerLogo=document.getElementsByClassName("hlogo"),mainContent=document.getElementsByClassName("notice_content")[0];if(null!=wordpressAdminBody&&notification.length>0&&null!=mainContent){var googleFont=document.createElement("link");googleFontHref=document.createAttribute("href"),googleFontRel=document.createAttribute("rel"),googleFontHref.value="https://fonts.googleapis.com/css?family=Roboto:300,400,600,700",googleFontRel.value="stylesheet",googleFont.setAttributeNode(googleFontHref),googleFont.setAttributeNode(googleFontRel);var css="@media only screen and (max-width: 576px) {#main_content {max-width: 320px !important;} #main_content h1 {font-size: 30px !important;} #main_content h2 {font-size: 40px !important; margin: 20px 0 !important;} #main_content p {font-size: 14px !important;} #main_content .content-wrapper {text-align: center !important;}} @media only screen and (max-width: 781px) {#main_content {margin: auto; justify-content: center; max-width: 445px;}} @media only screen and (max-width: 1325px) {.web-hosting-90-off-image-wrapper {position: absolute; max-width: 95% !important;} .notice_content {justify-content: center;} .web-hosting-90-off-image {opacity: 0.3;}} @media only screen and (min-width: 769px) {.notice_content {justify-content: space-between;} #main_content {margin-left: 5%; max-width: 445px;} .web-hosting-90-off-image-wrapper {position: absolute; display: flex; justify-content: center; width: 100%; }} .web-hosting-90-off-image {max-width: 90%;} .content-wrapper {min-height: 454px; display: flex; flex-direction: column; justify-content: center; z-index: 5} .notice_content {display: flex; align-items: center;} * {-webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale;} .upgrade_button_red_sale{box-shadow: 0 2px 4px 0 rgba(255, 69, 70, 0.2); max-width: 350px; border: 0; border-radius: 3px; background-color: #ff4546 !important; padding: 15px 55px !important; font-family: 'Roboto', sans-serif; font-size: 16px; font-weight: 600; color: #ffffff;} .upgrade_button_red_sale:hover{color: #ffffff !important; background: #d10303 !important;}",style=document.createElement("style"),sheet=window.document.styleSheets[0];style.styleSheet?style.styleSheet.cssText=css:style.appendChild(document.createTextNode(css)),document.getElementsByTagName("head")[0].appendChild(style),document.getElementsByTagName("head")[0].appendChild(googleFont);var button=document.getElementsByClassName("upgrade_button_red")[0],link=button.parentElement;link.setAttribute("href","https://www.hostinger.com/hosting-starter-offer?utm_source=000webhost&utm_medium=panel&utm_campaign=000-wp"),link.innerHTML='<button class="upgrade_button_red_sale">Go for it</button>',(notification=notification[0]).setAttribute("style","padding-bottom: 0; padding-top: 5px; background-color: #040713; background-size: cover; background-repeat: no-repeat; color: #ffffff; border-left-color: #040713;"),notification.className="notice notice-error is-dismissible";var mainContentHolder=document.getElementById("main_content");mainContentHolder.setAttribute("style","padding: 0;"),hostingerLogo[0].remove();var h1Tag=notification.getElementsByTagName("H1")[0];h1Tag.className="000-h1",h1Tag.innerHTML="Black Friday Prices",h1Tag.setAttribute("style",'color: white; font-family: "Roboto", sans-serif; font-size: 22px; font-weight: 700; text-transform: uppercase;');var h2Tag=document.createElement("H2");h2Tag.innerHTML="Get 90% Off!",h2Tag.setAttribute("style",'color: white; margin: 10px 0 15px 0; font-family: "Roboto", sans-serif; font-size: 60px; font-weight: 700; line-height: 1;'),h1Tag.parentNode.insertBefore(h2Tag,h1Tag.nextSibling);var paragraph=notification.getElementsByTagName("p")[0];paragraph.innerHTML="Get Web Hosting for $0.99/month + SSL Certificate for FREE!",paragraph.setAttribute("style",'font-family: "Roboto", sans-serif; font-size: 16px; font-weight: 700; margin-bottom: 15px;');var list=notification.getElementsByTagName("UL")[0];list.remove();var org_html=mainContent.innerHTML,new_html='<div class="content-wrapper">'+mainContent.innerHTML+'</div><div class="web-hosting-90-off-image-wrapper"><img class="web-hosting-90-off-image" src="https://cdn.000webhost.com/000webhost/promotions/bf-2020-wp-inject-img.png"></div>';mainContent.innerHTML=new_html;var saleImage=mainContent.getElementsByClassName("web-hosting-90-off-image")[0]}</script></body>
+</html>
+
